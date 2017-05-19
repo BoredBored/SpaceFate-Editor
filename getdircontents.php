c7297d4439518afc9652049fb4a16fdb4791fb5b
Alexios Angeletakis <alexiosangeletakis@yahoo.com>
2017-05-19 06:27:45 -0400

    Update getdircontents.php

--- getdircontents.php
+++ getdircontents.php
@@ -1,43 +1,44 @@
 <?php
 $path = $_REQUEST["q"];
+$orderPref = $_REQUEST["orderpreference"];
 
-if ($path == "")
-	{
+if ($path == "") {
 	$DirContentsArray = scandir("/var/www/admin");
 	$arrlength = count($DirContentsArray);
-	for ($x = 0; $x < $arrlength; $x++)
-		{
-		if ($DirContentsArray[$x] == ".")
-			{
-			echo "";
+	for ($x = 0; $x < $arrlength; $x++) {
+		if ($DirContentsArray[$x] == ".") {
+		}
+		elseif ($DirContentsArray[$x] == "..") {
+		}
+		else {
+			if (is_dir(DirContentsArray[$x]) == true) {
+				$dircontents = "${$dircontents}<li><div class=\"dir\" onclick=\"getdircontents();\"><p>${DirContentsArray[$x]}</p></div></li>";
 			}
-		elseif ($DirContentsArray[$x] == "..")
-			{
-			echo "";
-			}
-		  else
-			{
-			echo "<li><a href=\"http://admin.angeletakis/${DirContentsArray[$x]}\">${DirContentsArray[$x]}</a></li>";
+			else {
+				$dircontents = "${$dircontents}<li><div class=\"file\" onclick=\"openeditor();\"><p>${DirContentsArray[$x]}</p></div></li>";
 			}
 		}
 	}
-  else
-	{
+
+	echo $dircontents;
+}
+else {
 	$DirContentsArray = scandir($path);
 	$arrlength = count($DirContentsArray);
-	for ($x = 0; $x < $arrlength; $x++)
-		{
-		if ($DirContentsArray[$x] == ".")
-			{
-			echo "";
+	for ($x = 0; $x < $arrlength; $x++) {
+		if ($DirContentsArray[$x] == ".") {
+		}
+		elseif ($DirContentsArray[$x] == "..") {
+			echo "${$dircontents}<li><a href=\"http://admin.angeletakis/${path}/..\">Parent Directory</a></li>";
+		}
+		else {
+			if (is_dir(DirContentsArray[$x]) == true) {
+				$dircontents = "${$dircontents}<li><div class=\"dir\" onclick=\"openeditor();\"><p>${DirContentsArray[$x]}</p></div></li>";
 			}
-		elseif ($DirContentsArray[$x] == "..")
-			{
-			echo "<li><a href=\"http://admin.angeletakis/${path}/..\">Parent Directory</a></li>";
-			}
-		  else
-			{
-			echo "<li><a href=\"http://admin.angeletakis/${path}/${DirContentsArray[$x]}\">${DirContentsArray[$x]}</a>"echo "</li>";
+			else {
+				$dircontents = "${$dircontents}<li><div class=\"file\" onclick=\"openeditor();\"><p>${DirContentsArray[$x]}</p></div></li>";
 			}
 		}
 	}
+	echo $dircontents;
+}
