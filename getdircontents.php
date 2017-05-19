ebbf5aa1e392fa85059f1c38106180fece8bd42c
Alexios Angeletakis <alexiosangeletakis@yahoo.com>
2017-05-19 05:49:13 -0400

    Update getdircontents.php

--- getdircontents.php
+++ getdircontents.php
@@ -2,29 +2,42 @@
 $path = $_REQUEST["q"];
 
 if ($path == "")
-{
- $DirContentsArray = scandir("/var/www/admin");
- $arrlength = count($DirContentsArray);
- for ($x = 0; $x < $arrlength; $x++)
- {
-  if ($DirContentsArray[$x] == "."){
-      echo "";
-  } elseif ($DirContentsArray[$x] == "..")
-  {
-   echo "<li>";
-      echo "<a href=\"http://admin.angeletakis/${path}/..\">Parent Directory</a>";
-      echo "</li>";
-  }
+	{
+	$DirContentsArray = scandir("/var/www/admin");
+	$arrlength = count($DirContentsArray);
+	for ($x = 0; $x < $arrlength; $x++)
+		{
+		if ($DirContentsArray[$x] == ".")
+			{
+			echo "";
+			}
+		elseif ($DirContentsArray[$x] == "..")
+			{
+			echo "";
+			}
+		  else
+			{
+			echo "<li><a href=\"http://admin.angeletakis/${DirContentsArray[$x]}\">${DirContentsArray[$x]}</a></li>";
+			}
+		}
+	}
   else
-  {
-   echo "<li>";
-      echo "<a href=\"http://admin.angeletakis/${path}/${DirContentsArray[$x]}\">${DirContentsArray[$x]}</a>"
-   echo "</li>";
-  }
- }
-}
-else
-{
- $DirContentsArray = scandir($q);
- echo $dirContents;
-}
+	{
+	$DirContentsArray = scandir($path);
+	$arrlength = count($DirContentsArray);
+	for ($x = 0; $x < $arrlength; $x++)
+		{
+		if ($DirContentsArray[$x] == ".")
+			{
+			echo "";
+			}
+		elseif ($DirContentsArray[$x] == "..")
+			{
+			echo "<li><a href=\"http://admin.angeletakis/${path}/..\">Parent Directory</a></li>";
+			}
+		  else
+			{
+			echo "<li><a href=\"http://admin.angeletakis/${path}/${DirContentsArray[$x]}\">${DirContentsArray[$x]}</a>"echo "</li>";
+			}
+		}
+	}
