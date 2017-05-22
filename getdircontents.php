b136442837b7cb07ddc780101644b59075e6abad
Alexios Angeletakis <alexiosangeletakis@yahoo.com>
2017-05-21 20:23:04 -0400

    Update getdircontents.php

--- getdircontents.php
+++ getdircontents.php
@@ -2,28 +2,8 @@
 $path = $_REQUEST["q"];
 $orderPref = $_REQUEST["orderpreference"];
 
-if ($path == "") {
-	$DirContentsArray = scandir("/var/www/admin");
-	$arrlength = count($DirContentsArray);
-	for ($x = 0; $x < $arrlength; $x++) {
-		if ($DirContentsArray[$x] == ".") {
-		}
-		elseif ($DirContentsArray[$x] == "..") {
-		}
-		else {
-			if (is_dir(DirContentsArray[$x]) == true) {
-				$dircontents = "${$dircontents}<li><div class=\"dir\" onclick=\"getdircontents();\"><p>${DirContentsArray[$x]}</p></div></li>";
-			}
-			else {
-				$dircontents = "${$dircontents}<li><div class=\"file\" onclick=\"openeditor();\"><p>${DirContentsArray[$x]}</p></div></li>";
-			}
-		}
-	}
-
-	echo $dircontents;
-}
-else {
-	$DirContentsArray = scandir($path);
+if ($path != "") {
+	$DirContentsArray = scandir("/var/www/admin${path}");
 	$arrlength = count($DirContentsArray);
 	for ($x = 0; $x < $arrlength; $x++) {
 		if ($DirContentsArray[$x] == ".") {
@@ -41,4 +21,5 @@ else {
 		}
 	}
 	echo $dircontents;
-}
+} else {echo "error";
+       }
