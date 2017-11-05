efd2958781e9ad6b4d7b54e0d2cd1ac9a5554831
Alexios Angeletakis <alexiosangeletakis@yahoo.com>
2017-11-05 15:16:12 -0500

    Update gc.php

--- gc.php
+++ gc.php
@@ -1,6 +1,4 @@
 <?php
   //in js trim whitespace in order to insure faster server runtime
-$weburl = implode("", filter_var($_REQUEST["url"], FILTER_SANITIZE_URL));
-$webpageSrc = file_get_contents($weburl);
-echo $webpageSrc;
+echo file_get_contents(implode("", filter_var($_REQUEST["url"], FILTER_SANITIZE_URL)));
 ?>
