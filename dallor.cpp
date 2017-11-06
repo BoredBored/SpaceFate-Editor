9753f6b272ae1437007668b545263b6d65e45600
Alexios Angeletakis <alexiosangeletakis@yahoo.com>
2017-11-06 18:14:50 -0500

    Update dallor.cpp

--- dallor.cpp
+++ dallor.cpp
@@ -1,12 +1,66 @@
-<!DOCTYPE html>
-<html lang="en-US">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<script src="https://www.w3schools.com/lib/w3.js"></script>
-		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-	</head>
-	<body>
-		
-	</body>
-<html>
+#include <iostream>
+#include <string>
+using namespace std;
+
+unsigned char w = 24;
+string s = "$";
+string n = "\n";
+string c = " ";
+
+string g (unsigned char& w, string& buff) {
+  unsigned char t = w / 2 - buff.length () / 2;
+  string y = "";
+  for (unsigned char e = 0; e < t; e++)
+    {
+      y += c;
+    }
+  return s + y + buff + y + s + n;
+}
+
+string a ()
+{
+  string f = "";
+  for (unsigned char i = 0; i < w; i++)
+    {
+      f += s;
+    }
+  return s + n;
+}
+
+string z ()
+{
+  string innerText = "";
+  string p = c + s + s + s + c;
+  for (unsigned char o = 0; o < 2; o++)
+    {
+      string j = o ? c : s;
+      string k = o ? s : c;
+      for (unsigned char q = o ? 0 : 4; o ? q < 4 : q >= 0; o ? q++ : q--){
+	    string buff = "";
+	    switch (q){
+	        case 0:
+	        buff = c + c + s + c + c;
+	        break;
+	        
+	        case 1:
+	        buff = p;
+	        break;
+	        
+	        case 2:
+	        case 3:
+	        buff = j + c + s + c + k;
+	        break;
+	    }
+	    innerText += g(w, buff);
+	  }
+        innerText += o ? g(w, p) : "";
+    }
+    return innerText;
+}
+
+int main ()
+{
+  string b = a ();
+  cout << n + b + z() + b;
+  return 0;
+}
